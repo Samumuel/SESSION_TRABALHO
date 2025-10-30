4e268a5b9d9826eadbbe9d3ac92eba23e3a4e38b
Samuel Younis <134728703+Samumuel@users.noreply.github.com>
2025-10-30 17:39:12 -0300

    Delete trabalho_session directory

--- trabalho_session/view/excluir.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(__DIR__ . "/../control/PokemonControl.php");
-require_once(__DIR__ . "/../model/Pokemon.php");
-
-$pokemonControl = new PokemonControl();
-$result = $pokemonControl->apagarDadosSessao();
-
-header("Location: /LPW/trabalho-session/trabalho_session/index.php");
